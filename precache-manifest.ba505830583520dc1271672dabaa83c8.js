2ef8c90ef0c596ea8e6e4a2ed1e9521aeb953a8f
Liu Jie <jiel520@uw.edu>
2020-02-09 14:59:19 -0800

    Updates

---
+++ precache-manifest.ba505830583520dc1271672dabaa83c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2900fd65e1a9c8278e88aeb5ff34b6",
+    "url": "/BIMD362/index.html"
+  },
+  {
+    "revision": "3ad9bec697df59eeb7f1",
+    "url": "/BIMD362/static/css/main.83243b0f.chunk.css"
+  },
+  {
+    "revision": "7e97d5ce26ee513022cb",
+    "url": "/BIMD362/static/js/2.6f682e0a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BIMD362/static/js/2.6f682e0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ad9bec697df59eeb7f1",
+    "url": "/BIMD362/static/js/main.a6733009.chunk.js"
+  },
+  {
+    "revision": "2c20dc7cce3a4a187a86",
+    "url": "/BIMD362/static/js/runtime-main.28970290.js"
+  }
+]);
